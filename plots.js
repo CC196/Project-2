3a858083e6c4f731f0415008153d599ee5f532ec
CC196 <ccai.yangling@gmail.com>
2020-10-15 22:50:34 -0700

    more box plot

--- plots.js
+++ plots.js
@@ -4,26 +4,32 @@
     var dance2 = [];
 
 // read data  and fill each of the above array with data
+function get_first(feat,plotnumber){
 d3.json("http://127.0.0.1:5000//api/v1.0/top_50_2019").then(function(data){
-   for (var i=0; i < data.length; i++)
-   console.log(data[i]["danceability"])
-get_musicFeat(data)
+//    for (var i=0; i < data.length; i++)
+//    console.log(data[i][feat])
+    get_musicFeat(data,feat,plotnumber)
     // nineteen = data
-})
-function get_musicFeat(dancydata){    
+})}
+function get_musicFeat(dancydata,feat,plotnumber){    
 d3.json("http://127.0.0.1:5000//api/v1.0/top_50_2020_May").then(function(data){
     for (var i=0; i < data.length; i++){
-        dance1.push(dancydata[i]["danceability"]);
-        dance2.push(data[i]["danceability"]*100);
+        dance1.push(dancydata[i][feat]);
+        if (feat === "loudness"){
+            dance2.push(data[i][feat])
+        }else{
+            dance2.push(data[i][feat]*100);
+        }
+        
     
-    console.log(data[i]["danceability"]*100)
+    // console.log(data[i][feat]*100)
     
     }
-get_dancebox(dance1,dance2)
+get_dancebox(dance1,dance2,feat,plotnumber)
 
 });
 } 
-function get_dancebox(dance1,dance2){
+function get_dancebox(dance1,dance2,feat,plotnumber ){
 // Create a trace object with the data in `dance1`
     var trace1 = {
     y: dance1,
@@ -45,11 +51,14 @@ function get_dancebox(dance1,dance2){
 
  // Use `layout` to define a title
     var layout = {
-     title: "Danceability Comparison btwn 2019 - 2020"
+     title: `${feat} comparison btwn 2019 - 2020`
   };   
 
 // Render the plot to the `plot1` div
-    Plotly.newPlot("plots", danceFeat, layout);
+    Plotly.newPlot(plotnumber, danceFeat, layout);
 }
 
 
+get_first("danceability","plot1")
+get_first("valence","plot2")
+get_first("loudness","plot3")
